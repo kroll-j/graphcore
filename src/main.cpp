b889ef6a11565336a8a49dd113c4ab064eff1633
Johannes Kroll <jkroll@lavabit.com>
2011-05-16 17:04:40 +0200

    add clibase.h

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,12 @@
 
 using namespace std;
 
+#include "clibase.h"
 
+
+#ifndef _
 #define _(string) gettext(string)
+#endif
 
 #define U32MAX (0xFFFFFFFF)
 
@@ -606,63 +610,7 @@ class Digraph
 };
 
 
-// convenience macros for printing success/failure/error messages from the cli.
-// if the protocol for those messages should ever have to be modified, change these.
-#define SUCCESS_STR "OK."
-#define FAIL_STR "FAILED!"
-#define ERROR_STR "ERROR!"
-#define NONE_STR "NONE."
-#define cliMessage(str, x...) ({ char c[2048]; int n= sprintf(c, str " "); snprintf(c+n, sizeof(c)-n, x); lastStatusMessage= c; })
-#define cliSuccess(x...) cliMessage(SUCCESS_STR, x)
-#define cliFailure(x...) cliMessage(FAIL_STR, x)
-#define cliError(x...) cliMessage(ERROR_STR, x)
-#define cliNone(x...) cliMessage(NONE_STR, x)
 
-enum CommandStatus
-{
-    CMD_SUCCESS= 0,
-    CMD_FAILURE,
-    CMD_ERROR,
-    CMD_NONE,
-};
-
-// abstract base class for cli commands
-class CliCommand
-{
-    public:
-        enum ReturnType
-        {
-            RT_NONE,
-            RT_ARC_LIST,
-            RT_NODE_LIST,
-            RT_OTHER,
-        };
-
-        CliCommand() { }
-        virtual ~CliCommand() { }
-
-        // the command name
-        virtual string getName()            { return "CliCommand"; }
-        // one line describing the command and its parameters
-        virtual string getSynopsis()        { return getName(); }
-        // help text describing the function of the command
-        virtual string getHelpText()        { return "Help text for " + getName() + "."; }
-        void syntaxError()
-        {
-            lastStatusMessage= string(FAIL_STR) + _(" Syntax: ") + getSynopsis() + "\n";
-            if(getReturnType()==RT_OTHER) cout << lastStatusMessage;
-        }
-        const string &getStatusMessage()    { return lastStatusMessage; }
-        virtual ReturnType getReturnType()= 0;
-
-        // read a data set of node IDs.
-        // expectedSize: expected size of set per line (e. g. 1 for nodes, 2 for arcs)
-        // update lastErrorString and return true on success, false on failure.
-        bool readNodeset(FILE *inFile, vector< vector<uint32_t> > &dataset, unsigned expectedSize);
-
-    protected:
-        string lastStatusMessage;
-};
 
 
 // base classes for cli commands. derive commands from these.
@@ -738,7 +686,7 @@ class Cli
                 if(outRedir) fclose(outRedir), outRedir= 0;
                 if( (command= getLine())==0 ) return;
                 char *completeCommand= strdup(command);
-                if(!completeCommand) { printf("ERROR! out of memory.\n"); return; }
+                if(!completeCommand) { printf(ERROR_STR " out of memory.\n"); return; }
                 char *d= strchr(command, '>');
                 if(d)
                 {
@@ -1016,7 +964,7 @@ class Cli
 // read a data set of node IDs.
 // expectedSize: expected size of set per line (e. g. 1 for nodes, 2 for arcs)
 // update lastErrorString and return true on success, false on failure.
-bool CliCommand::readNodeset(FILE *inFile, vector< vector<uint32_t> > &dataset, unsigned expectedSize)
+inline bool CliCommand::readNodeset(FILE *inFile, vector< vector<uint32_t> > &dataset, unsigned expectedSize)
 {
     vector<uint32_t> record;
     bool ok= true;
